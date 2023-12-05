d041b8d9802f87fffaf07842148dac5da9999485
Pratik Patel <150986560+PatelPratik39@users.noreply.github.com>
2023-12-05 00:45:44 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+// console.log("Have a Nice Day!!!");
+
+// function displayAlert(){
+//     alert("Have a nice day!!");
+// }
+
+// let num = 4;
+// console.log("Its old new Number : " + num);
+
+// num =9;
+// console.log("Its my new Number : " + num);
+
+
+// const character = 'p';
+// console.log("Its my Character : " + character);
+
+// // const character = 'B';
+// console.log("Its my new Character : " + character);
+
+// const str1 = "PrAtik"
+// const str2 = "PatEL"
+// console.log(" Welcome to the Full stack Academy : " + str1 + " " + str2);
+
+// const strComplete = `Welcome to ${str1} ${str2}'s World!!!!`;
+// console.log(strComplete);
+
+/*
+Operators ==============
+*/
+
+// ARITHMATIC OPERATORS : ' +, -, /, * '
+// let sum = 0;
+// console.log(sum, 'I am sum');
+// sum = 1 + 2;
+// console.log(sum, 'I am addition');
+// sum2 = 1 - 2;
+// console.log(sum2 , 'I am subtraction');
+// sum3 = 1 * 2;
+// console.log(sum3);
+// sum4 = 10 / 2;
+// console.log(sum4);
+// sum5 = 30 % 7;
+// console.log(sum5);
+
+// console.log(typeof sum4);
+
+/*
+    Week 04 - Guided Practice for JavaScript
+*/
+
+// Pseudocode practice
+
+/*
+
+    1. Grab a toothbrush
+    2. Turn on water from tap
+    3. Wash toothbrush
+    4. Turn off Water from tap 
+    5. Open the cap of tootpaste
+    6. Press the back side of toothpaste so toothpast can come out from open end
+    7. Put the toothpast on toothbrush
+    8. Start brushing the teeth in circular motion
+*/
+
+// Declaring a variable
+var numOfCows = 50;
+
+// printing a string in console
+const str = "Hello World!!"
+console.log(str);
+
+// concatinating a string without " +" icon
+let newStr = `Hello World!!! there are ${numOfCows} gazing in field`;
+console.log(newStr);
+
+
+// Arithmatic Operators
+const umbrellas = 10 * 3;
+console.log(umbrellas);
+
+// Addition using Arithmatic Operator
+let num = 5 + 1;
+console.log(num);
+
+// Add an external script file to an HTML file.
+{/* <script src="index.js"></script> */}
+
+// concatinating a string
+let str2 = 'Hello'; 
+let str3 = 'Full stack';
+let str4 = 'Academy';
+console.log(str + " " +str2 + " " + str3 + " "+ str4);
+
+
+// Print three strings in alert box
+let str5 = "Hello,";
+let str6 = "I am";
+let str7 = "Pratik !!";
+alert(str5 + " " + str6 + " " + str7);
+
